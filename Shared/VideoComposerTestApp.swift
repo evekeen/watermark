11a77cb8034bc1897e8a6f7b6acc3948daf05a86
Alexander Ivkin <a@ivkin.dev>
2022-06-18 09:25:31 -0400

    Initial Commit

---
+++ Shared/VideoComposerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoComposerTestApp.swift
+//  Shared
+//
+//  Created by Alexander Ivkin on 6/18/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoComposerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
